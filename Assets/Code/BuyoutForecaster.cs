54563379da2c83b3c790e1e9699ca496cc5c0cd0
Dave <45405824+DavidKoleczek@users.noreply.github.com>
2022-04-04 18:21:06 -0400

    buyout forecast price

---
+++ Assets/Code/BuyoutForecaster.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyoutForecaster : MonoBehaviour
+{
+    public int BUYOUT_EXTRAPOLATION_DAYS = 10;
+    public int BUYOUT_DAYS_TO_ESTABLISH_SELF = 25;
+    public int BUYOUT_LAST_DAYS_FOR_AVG = 15;
+    public float BUYOUT_DEFAULT_PRICE = 100f;
+    public int BUYOUT_DEFAULT_FIRST_DAYS = 5;
+
+    public float GetBuyoutAmount(Dictionary<int, List<float>> data)
+    {
+        //sum each list in data to get total daily income (or loss)
+        List<float> daily_income = new List<float>();
+        foreach (KeyValuePair<int, List<float>> entry in data)
+        {
+            float income = 0f;
+            foreach (float price in entry.Value)
+            {
+                income += price;
+            }
+            daily_income.Add(income);
+        }
+
+        // If there are less than BUYOUT_DEFAULT_FIRST_DAYS days of data, return BUYOUT_DEFAULT_PRICE
+        if (daily_income.Count < BUYOUT_DEFAULT_FIRST_DAYS)
+        {
+            return BUYOUT_DEFAULT_PRICE;
+        }
+
+        //Get last BUYOUT_LAST_DAYS_FOR_AVG values in daily_income
+        List<float> daily_income_last = new List<float>();
+        for (int i = Mathf.Max(0, daily_income.Count - 50); i < daily_income.Count; ++i)
+        {
+            daily_income_last.Add(daily_income[i]);
+        }
+
+        //calculate average of daily income for last BUYOUT_LAST_DAYS_FOR_AVG
+        float average_income = 0f;
+        foreach (float income in daily_income_last)
+        {
+            average_income += income;
+        }
+        average_income /= daily_income_last.Count;
+
+        //Buyout formula: average sales over last 10 days * Clamp01(curr day / BUYOUT_DAYS_TO_ESTABLISH_SELF) * BUYOUT_EXTRAPOLATION_DAYS
+        float buyout_price = average_income * Mathf.Clamp01(daily_income.Count / BUYOUT_DAYS_TO_ESTABLISH_SELF) * BUYOUT_EXTRAPOLATION_DAYS;
+        return buyout_price;
+    }
+}
