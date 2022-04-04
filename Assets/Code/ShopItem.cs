907d8c5933b3b9b0f87e3ff11561506f927d76ce
mfclinton <mfclinton0@gmail.com>
2022-04-04 16:13:09 -0400

    shop auto update text

--- Assets/Code/ShopItem.cs
+++ Assets/Code/ShopItem.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine.Events;
 
 public class ShopItem : MonoBehaviour
 {
     public float cost;
+    public TextMeshProUGUI cost_text;
     public UnityEvent invoked_method;
+
     GameManager gm;
 
     private void Start()
     {
         gm = FindObjectOfType<GameManager>();
+
+        string pre_text = "";
+        if (cost < 0f)
+            pre_text = "-";
+
+        cost_text.text = pre_text + "$" + Mathf.Abs(cost).ToString("0.0");
     }
 
     public void Buy()
